927d0d46fce42b47634a4a417766e158a51afb06
PAVAN ALAPATI <2200031028@kluniversity.in>
2025-02-04 22:05:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/oxapavan.github.io/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/oxapavan.github.io/', // Replace with your GitHub repo name
 });
