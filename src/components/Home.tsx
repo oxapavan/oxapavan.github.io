2748b2708d3285d06c7e8945a64606897443ae01
PAVAN ALAPATI <2200031028@kluniversity.in>
2025-02-04 22:50:43 +0530

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,8 +15,12 @@ function Home() {
         <div className="max-w-2xl mx-auto">
           <p className="text-lg text-gray-400 mb-6">
           Just another Security Enthusiast Kid
+          </p>
+          
+          <p className="text-lg text-gray-400 mb-6">
           I’m Pavan Alapati (aka AngryBird), a third-year undergraduate specializing in Computer Science (Cybersecurity) at KL University. My passion lies in Red Teaming and Threat Hunting, with a focus on Privilege Escalation, Defense Evasion, Credential Access, Lateral Movement, and other offensive security mechanisms.
           </p>
+          
           <p className="text-lg text-gray-400">
           On my blog, I share CTF writeups, CVE findings, and other insights from my journey. Stick around and explore what I’m up to—I’m glad you’re here!
           
