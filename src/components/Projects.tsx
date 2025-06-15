8a0299c14dc4a314c0ccf81ed99a5ffe052e9964
Pavan Alapati <2200031028@kluniversity.in>
2025-06-15 22:04:05 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -91,11 +91,9 @@ function Projects() {
                 <span>Code</span>
               </a>
               <a
-                href={project.demo}
                 className="flex items-center text-cyan-500 hover:text-cyan-400"
               >
                 <ExternalLink className="w-5 h-5 mr-1" />
-                <span>Demo</span>
               </a>
               <button
                 onClick={() => setSelectedProject(project)}
